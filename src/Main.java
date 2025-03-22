ecfa9e913e5ac2852e0c12b412519a778f1d87da
HagataMendes <hagatamendes2017@gmail.com>
2025-03-22 12:56:22 -0300

    TiposConverte

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    //Calculadora
+//    Scanner scanner = new Scanner(System.in);
+//
+//
+//        System.out.print("Digite o primeiro número: ");
+//    double numero1 = Double.parseDouble(scanner.nextLine());
+//
+//        System.out.print("Digite o segundo número: ");
+//    double numero2 = Double.parseDouble(scanner.nextLine());
+//
+//        System.out.println("Soma: "+(numero1 +numero2));
+//        System.out.println("Subtração: "+(numero1 -numero2));
+//        System.out.println("Multiplicação: "+(numero1 *numero2));
+//        System.out.println("Divisão: "+(numero1 /numero2));
+//
+//        //---------------------------------------------------
+//
+//        System.out.println();
+
+        //Detectando Estouro de Variavel
+//
+//        byte valorByte = 127;
+//
+//
+//        System.out.println("Valor inicial: " + valorByte);
+//        valorByte++;
+//        System.out.println("Após adicionar 1: " + valorByte);
+//        valorByte++;
+//        System.out.println("Após adicionar mais 1: " + valorByte);
+
+
+
+        //Valores Flutuantes
+        float valorFloat = 10.5F;
+        double valorDouble = 20.5;
+
+        System.out.println("Valor float: " + valorFloat);
+        System.out.println("Valor double: " + valorDouble);
+
+        // Operação
+        double resultado = valorFloat + valorDouble;
+        System.out.println("Resultado da soma: " + resultado);
+    }
+}
+
+
+
